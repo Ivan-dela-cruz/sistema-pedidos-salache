aa14a2aca94366cd91c2be8580db3ecb479fb240
Ivan-dela-cruz <canarivan495@gmail.com>
2020-08-27 14:50:35 -0500

    pdf

--- resources/views/web/categories.blade.php
+++ resources/views/web/categories.blade.php
@@ -3,110 +3,126 @@
 @section('content')
 
 
-<main id="main">
+    <main id="main">
 
 
-    <!-- ======= Breadcrumbs ======= -->
-    <section id="breadcrumbs" class="breadcrumbs">
-        <div class="container">
+        <!-- ======= Breadcrumbs ======= -->
+        <section id="breadcrumbs" class="breadcrumbs">
+            <div class="container">
 
-          <div class="d-flex justify-content-between align-items-center">
-            <h2>Categorias</h2>
-            <ol>
-              <li><a href="{{route('index')}}">Inicio</a></li>
-              <li>Categorias</li>
-            </ol>
-          </div>
-
-        </div>
-      </section><!-- End Breadcrumbs -->
-
-      <!-- ======= Services Section ======= -->
-      <section id="services" class="services section-bg">
-        <section id="team" class="team section-bg">
-          <div class="container">
-
-
-            <div class="row">
-
-              <div class="col-lg-4 col-md-2 d-flex align-items-stretch">
-                <div class="member" data-aos="fade-up" data-aos-delay="100">
-                  <div class="member-img">
-                  <img src="{{asset('assets2/img/categorias2.jpg')}}" class="img-fluid" alt="">
-
-                  </div>
-                  <div class="member-info">
-                    <h4>Alimentos</h4>
-                    <span></span>
-                  </div>
+                <div class="d-flex justify-content-between align-items-center">
+                    <h2>Categorias</h2>
+                    <ol>
+                        <li><a href="{{route('index')}}">Inicio</a></li>
+                        <li>Categorias</li>
+                    </ol>
                 </div>
-              </div>
-              <div class="col-lg-4 col-md-5 d-flex align-items-stretch">
-                <div class="member" data-aos="fade-up" data-aos-delay="100">
-                  <div class="member-img">
-                    <img src="{{asset('assets2/img/categorias3.jpg')}}" class="img-fluid" alt="">
-
-                  </div>
-                  <div class="member-info">
-                    <h4>Ropa y Accesorios</h4>
-                    <span></span>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-lg-4 col-md-5 d-flex align-items-stretch">
-                <div class="member" data-aos="fade-up" data-aos-delay="200">
-                  <div class="member-img">
-                    <img src="{{asset('assets2/img/categorias5.jpg')}}" class="img-fluid" alt="">
-
-                  </div>
-                  <div class="member-info">
-                    <h4>Transporte</h4>
-                    <span></span>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-4 col-md-5 d-flex align-items-stretch">
-                <div class="member" data-aos="fade-up">
-                  <div class="member-img">
-                    <img src="{{asset('assets2/img/categorias1.jpg')}}" class="img-fluid" alt="">
-
-                  </div>
-                  <div class="member-info">
-                    <h4>Equipos  de bioseguridad  e insumos de salud</h4>
-                    <span> </span>
-                  </div>
-                </div>
-
-              </div>
-
-              <div class="col-lg-4 col-md-5 d-flex align-items-stretch">
-                <div class="member" data-aos="fade-up">
-                  <div class="member-img">
-                    <img src="{{asset('assets2/img/categorias4.jpg')}}" class="img-fluid" alt="">
-
-                  </div>
-                  <div class="member-info">
-                    <h4>Miscelaneos</h4>
-                    <span> </span>
-                  </div>
-                </div>
-
-              </div>
-
 
             </div>
+        </section><!-- End Breadcrumbs -->
+
+        <!-- ======= Services Section ======= -->
+        <section id="services" class="services section-bg">
+            <section id="team" class="team section-bg">
+                <div class="container">
 
 
-          </div>
-        </section>
-      </section><!-- End Services Section -->
+                    <div class="row">
 
-      <!-- ======= Features Section ======= -->
-      <section id="features" class="features">
+                        <div class="col-lg-4 col-md-2 d-flex align-items-stretch">
+                            <div class="member" data-aos="fade-up" data-aos-delay="100">
+                                <div class="member-img">
+                                    <img src="{{asset('assets2/img/categorias2.jpg')}}" class="img-fluid" alt="">
 
-      </section><!-- End Features Section -->
+                                </div>
+                                <div class="member-info">
+                                    <h4>Alimentos</h4>
+                                    <span></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-5 d-flex align-items-stretch">
+                            <div class="member" data-aos="fade-up" data-aos-delay="100">
+                                <div class="member-img">
+                                    <img src="{{asset('assets2/img/categorias3.jpg')}}" class="img-fluid" alt="">
 
-</main>
+                                </div>
+                                <div class="member-info">
+                                    <h4>Ropa y Accesorios</h4>
+                                    <span></span>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <div class="col-lg-4 col-md-5 d-flex align-items-stretch">
+                            <div class="member" data-aos="fade-up">
+                                <div class="member-img">
+                                    <img src="{{asset('assets2/img/turismo.jpeg')}}" class="img-fluid" alt="">
+
+                                </div>
+                                <div class="member-info">
+                                    <h4>Turismo</h4>
+                                    <span> </span>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="col-lg-4 col-md-5 d-flex align-items-stretch">
+                            <div class="member" data-aos="fade-up">
+                                <div class="member-img">
+                                    <img src="{{asset('assets2/img/categorias1.jpg')}}" class="img-fluid" alt="">
+
+                                </div>
+                                <div class="member-info">
+                                    <h4>Equipos de bioseguridad e insumos de salud</h4>
+                                    <span> </span>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="col-lg-4 col-md-5 d-flex align-items-stretch">
+                            <div class="member" data-aos="fade-up">
+                                <div class="member-img">
+                                    <img src="{{asset('assets2/img/categorias4.jpg')}}" class="img-fluid" alt="">
+
+                                </div>
+                                <div class="member-info">
+                                    <h4>Miscelaneos</h4>
+                                    <span> </span>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="col-lg-4 col-md-5 d-flex align-items-stretch">
+                            <div class="member" data-aos="fade-up" data-aos-delay="200">
+                                <div class="member-img">
+                                    <img src="{{asset('assets2/img/categorias5.jpg')}}" class="img-fluid" alt="">
+
+                                </div>
+                                <div class="member-info">
+                                    <h4>Transporte</h4>
+                                    <span></span>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+
+                </div>
+            </section>
+        </section><!-- End Services Section -->
+
+        <!-- ======= Features Section ======= -->
+        <section id="features" class="features">
+
+        </section><!-- End Features Section -->
+
+    </main>
 
 @endsection
