5c732313a160df53fca3a067a644872c1eb2df58
ivandelacruz <canarivan495@gmail.com>
2020-09-14 08:39:41 -0500

    pk forzar

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -56,6 +56,26 @@ class WebController extends Controller
         return view('admin.messages.index',compact('messages'));
     }
 
+    public function downloadapk(){
+        $fileName = basename('tiendavirtualutc.apk');
+        $filePath = 'apk/'.$fileName;
+        if(!empty($fileName) && file_exists($filePath)){
+            // Define headers
+            header("Cache-Control: public");
+            header("Content-Description: File Transfer");
+            header("Content-Disposition: attachment; filename=$fileName");
+            header("Content-Type: application/zip");
+            header("Content-Transfer-Encoding: binary");
+            
+            // Read the file
+            readfile($filePath);
+            exit;
+        }else{
+            echo 'The file does not exist.';
+        }
+    }
+
+/*
      public function downloadapk()
     {
        $url_apk= "apk/tiendavirtualutc.apk";
@@ -64,4 +84,5 @@ class WebController extends Controller
         
        
     }
+    */
 }
